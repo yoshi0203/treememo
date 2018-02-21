c0a9690e652ccc5c4d017fced638e540a47ba8d1
yoshitoyamanka <yoshito.y@Yoshitos-MacBook-Pro.local>
2018-02-21 22:28:59 +0900

    init

---
+++ MemoTree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemoTree
+//
+//  Created by 山中力仁 on 2018/02/20.
+//  Copyright © 2018年 yamayoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
